2b73f3497f7fe87e70a2ae761ae5964f5e57f2c2
sakib3046 <sakib3046@gmail.com>
2024-05-20 13:44:35 +0600

    Navbar created

---
+++ src/routes/components/navbar.svelte
@@ -0,0 +1,23 @@
+<script land="ts">
+	let navOptions = [
+		{ name: 'home', link: '/' },
+		{ name: 'Services', link: '/services' }
+	];
+</script>
+
+<div
+	class=" mx-8 my-4 flex flex-row justify-between w-full bg-gradient-to-r from-400 via-200 to-600 px-6 py-3 shadow-xl shadow-950/10 rounded-lg"
+>
+	<a href="/" class=" flex flex-col leading-3 hover:scale-110 duration-200">
+		<span class="font-bold">Edistys</span>
+		<span class="">Intern</span>
+	</a>
+	<div class="space-x-4 flex flex-row text-50 font-bold text-xl">
+		{#each navOptions as option}
+			<a
+				class="hover:text-white hover:-translate-y-0.5 hover:scale-110 duration-200"
+				href={option.link}>{option.name}</a
+			>
+		{/each}
+	</div>
+</div>
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    colors: {
+      '50': '#ebfef7',
+      '100': '#cffce9',
+      '200': '#a4f6d7',
+      '300': '#69ecc3',
+      '400': '#2bd9a8',
+      '500': '#09c092',
+      '600': '#009c78',
+      '700': '#007d63',
+      '800': '#026350',
+      '900': '#035142',
+      '950': '#002e27',
+    },
+    extend: {}
+  },
+  plugins: []
+};
