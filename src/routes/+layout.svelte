d7ce9c088fba1c538bc73de0dbdffe6eb187fc9d
sakib3046 <sakib3046@gmail.com>
2024-05-30 02:21:11 +0600

    Footer 80% Done

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,11 @@
 <script>
 	import '../app.css';
+	import Footer from './components/footer.svelte';
 	import Navbar from './components/navbar.svelte';
 </script>
 
 <Navbar />
-
 <slot />
+<footer class="">
+	<Footer />
+</footer>
